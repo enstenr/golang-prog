c9ff5cf3d43b6654f0c1e578ec1fd5bffe6dbef5
enstenr <enstenr@gmail.com>
2023-01-04 09:11:58 +0530

    removed token

--- l2-entities/load-entities.go
+++ l2-entities/load-entities.go
@@ -25,7 +25,7 @@ func loadEntities(metadataTreeConfigId string,l2_categories_map map[int64]map[st
 	req_obj, _ := json.Marshal(request_body)
 
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(req_obj))
-    req.Header.Set("Authorization", "Bearer eyJ0eXAiOiAiSldUIiwgImFsZyI6ICJSUzI1NiIsICJraWQiOiAiZDkwYzViNGU3OGI0OTE1YzEyYmU0YmJlOTk2MTdmZGU0MGY5NDU5MyJ9.eyJpYXQiOiAxNjcwMjMwNzU3LCAiZXhwIjogMTY3MDIzNDM1NywgImlzcyI6ICI1NzM0NDU2OTYxMTEtY29tcHV0ZUBkZXZlbG9wZXIuZ3NlcnZpY2VhY2NvdW50LmNvbSIsICJhdWQiOiAiaHR0cHM6Ly9rZy1kZXYuZW5kcG9pbnRzLm1lcmNhcmktdXMtZGUuY2xvdWQuZ29vZyIsICJzdWIiOiAiNTczNDQ1Njk2MTExLWNvbXB1dGVAZGV2ZWxvcGVyLmdzZXJ2aWNlYWNjb3VudC5jb20iLCAiZW1haWwiOiAiNTczNDQ1Njk2MTExLWNvbXB1dGVAZGV2ZWxvcGVyLmdzZXJ2aWNlYWNjb3VudC5jb20ifQ.QZLoTnQGDnI0VzUuhYVa0Npi9TMIxF_HU849xIkkeq2z0-HlIvYBtQwf0kY2wAGZ8lEpWA25MGO_Sw7TTGHNI3K30hW5R81li8bnScO_k1DQy2mj4KP4sO9sgddDLnPSPzCg1ncxjhHQQ8u5BRHjTVGL2mLgFW2RwyHkwTo7HuKJKSl1xyc3vIGNpPYira2HCLycGuJ_hU1PLsW4glQhKNDvm4IFaB2pKpC0lGjbtDV3zW4tnG1iXqCEbDpCcFuJkzZlWW6yZPVe_TRC2cZ-SdFjVRifipyOODQ8xjDw7IRqGZn91jFGdYlo3SzbAvoy9cCWQz25JMbevOOln9cVUw")
+    req.Header.Set("Authorization", "Bearer <<TOKEN HERE >>")
     req.Header.Set("Content-Type", "application/json; charset=UTF-8")
 
 
