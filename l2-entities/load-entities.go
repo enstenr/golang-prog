63c2395aeb5b37a7dee29b3a4052b2b28b78e583
enstenr <enstenr@gmail.com>
2023-05-23 08:52:38 +0530

    removed token

--- l2-entities/load-entities.go
+++ l2-entities/load-entities.go
@@ -26,7 +26,7 @@ func loadEntities(metadataTreeConfigId string, l2_categories_map map[int64]map[s
 	req_obj, _ := json.Marshal(request_body)
 
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(req_obj))
-	req.Header.Set("Authorization", "Bearer eyJ0eXAiOiAiSldUIiwgImFsZyI6ICJSUzI1NiIsICJraWQiOiAiMzk4OTViZjhjZTA5MWI2MWNlMjg2Yzc2OTYxYjdmMjAxYWNlMjNhNCJ9.eyJpYXQiOiAxNjg0MjI5NDAyLCAiZXhwIjogMTY4NDIzMzAwMiwgImlzcyI6ICI1NzM0NDU2OTYxMTEtY29tcHV0ZUBkZXZlbG9wZXIuZ3NlcnZpY2VhY2NvdW50LmNvbSIsICJhdWQiOiAiaHR0cHM6Ly9rZy1zdGFnZS5lbmRwb2ludHMubWVyY2FyaS11cy1kZS5jbG91ZC5nb29nIiwgInN1YiI6ICI1NzM0NDU2OTYxMTEtY29tcHV0ZUBkZXZlbG9wZXIuZ3NlcnZpY2VhY2NvdW50LmNvbSIsICJlbWFpbCI6ICI1NzM0NDU2OTYxMTEtY29tcHV0ZUBkZXZlbG9wZXIuZ3NlcnZpY2VhY2NvdW50LmNvbSJ9.DVr1uTh3ntmHJmX8qAaiSe4YwX5Cpn3Hsc5fdtj5npRd5-hu5ujEoPq-FUospz_G5kRsF906zlJWDe34iFwKL6-Fq9jycSze15Z1WHC6SBU_9n6-9EDSS2xEE5vAgcJnu4fHLwL2FHULhzjwZd5ojXQgYpng2E6P8dHAaoJ71_IihKJBsWsVRfXK4tmV0OoSCO7JF78IoSmOk3OZxQQE3jVQcwgAo677K6l68YhWYaChGNWKqjC8-Kj8qTkPCEhZvYmr2EffwDmNwzWieMiKpU4ixMFkWKX9paS6zrQFEhfYmaf-SttuWt2-GGrhlLA5Gp2S1zfubT1RqzROlslFBg")
+	req.Header.Set("Authorization", "Bearer <<TOKEN>>")
 	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
 
 	client := &http.Client{}
@@ -43,7 +43,7 @@ func loadEntities(metadataTreeConfigId string, l2_categories_map map[int64]map[s
 	if err := json.Unmarshal(body, &result); err != nil { // Parse []byte to go struct pointer
 		fmt.Println("Can not unmarshal JSON obj")
 	}
-	 
+
 	//fmt.Print(" Entities : ")
 	//fmt.Print(result.Response)
 	for _, value := range result.Response {
